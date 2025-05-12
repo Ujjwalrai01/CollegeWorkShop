eb7999b191456dd5ea5831b48ba04e7dec0506e6
Ujjwal Rai <urai31382@gmail.com>
2025-05-12 14:46:32 +0530

    Job

--- JobBoard/src/App.jsx
+++ JobBoard/src/App.jsx
@@ -5,6 +5,10 @@ import Home from './Component/NavBar/HomePage/Home.jsx';
 import Login from './Component/Login/Login.jsx';
 import Signup from './Component/SignUp/SignUp.jsx';
 import Dashboard from './Component/Dashboard/Dashboard.jsx';
+// import Dashboard from './Component/Dashboard/Dashboard.jsx';
+// import NavBar from './Component/NavBar/NavBar.jsx';
+// import Footer from './Component/Footer/Footer.jsx';
+// import { AuthProvider } from './Context/AuthContext.jsx'
 
 function App() {
 
@@ -15,6 +19,7 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/dashboard" element={<Dashboard />} />
+
     </Routes>
     </>
   )
