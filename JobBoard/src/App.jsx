a734b5b9bb52fd00203a7824d1dcd291f0d30040
Ujjwal Rai <urai31382@gmail.com>
2025-05-18 15:33:37 +0530

    Update App.jsx

--- JobBoard/src/App.jsx
+++ JobBoard/src/App.jsx
@@ -1,24 +1,23 @@
 import { useState } from 'react'
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
-import Home from './Component/NavBar/HomePage/Home.jsx';
-import Login from './Component/Login/Login.jsx';
-import Signup from './Component/SignUp/SignUp.jsx';
-import Dashboard from './Component/Dashboard/Dashboard.jsx';
+// import Home from './Component/NavBar/HomePage/Home.jsx';
+// import Login from './Component/Login/Login.jsx';
+// import Signup from './Component/SignUp/SignUp.jsx';
 // import Dashboard from './Component/Dashboard/Dashboard.jsx';
-// import NavBar from './Component/NavBar/NavBar.jsx';
-// import Footer from './Component/Footer/Footer.jsx';
-// import { AuthProvider } from './Context/AuthContext.jsx'
+import LandingPage from './pages/LandingPage.jsx';
+
 
 function App() {
 
   return (
     <>
       <Routes>
-      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/dashboard" element={<Dashboard />} /> */}
+      <Route path='/' element={<LandingPage />} />
 
     </Routes>
     </>
